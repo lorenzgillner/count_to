3b527fcc50e060092a444bb94b8358fa56ab6ca0
Lorenz Gillner <nospampls@aaathats3as.com>
2019-10-09 22:42:18 +0200

    various bug fixes

--- __init__.py
+++ __init__.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
-
 import sys
-from termcolor import colored
 import numpy
+
+from termcolor import colored
+
 
 #count array
 numbers = [
@@ -110,42 +111,54 @@ numbers = [
     "100"
 ]
 
+
 sortnumber = []
+
 
 def quicksort(arr):
     if len(arr) <= 1:
         return arr
+
     pivot = arr[len(arr) // 2]
     left = [x for x in arr if x < pivot]
     middle = [x for x in arr if x == pivot]
     right = [x for x in arr if x > pivot]
+    
     return quicksort(left) + middle + quicksort(right)
 
-def print():
-    #print counted numbers in array
+
+def print1():
+    # print counted numbers in array
     for number in range(len(numbers)):
         output = ""
+        
         for n in range(len(numbers[number])):
             output = str(output + str(numbers[number][n]))
-        #print in terminal
-        print colored("{}".format(int(output)),"red")
+    
+        # print in terminal
+        print(colored(f"{str(int(output))}", "red"))
+
 
 def print2():
     sortnumber = quicksort(numbers)
 
-    #print counted numbers in array
+    # print counted numbers in array
     for sortnumber in range(len(sortnumber)):
         output = ""
+        
         for n in range(len(numbers[sortnumber])):
             output = str(output + str(numbers[sortnumber][n]))
-        #print in terminal
-        print colored("{}".format(int(output)),"red")
+        
+        # print in terminal
+        print(colored(f"{str(int(output))}", "red"))
+
 
 def main():
-    print()
+    print1()
     print2()
 
 
 if __name__ == "__main__":
     main()
     sys.exit()
+
