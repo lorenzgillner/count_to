410ce39589d9b4fc75b33b477b5d62af201b19e8
Askill <ofjokg@gmail.com>
2019-10-11 20:31:35 +0200

    made it 'merican 🔥

--- __init__.py
+++ __init__.py
@@ -3,117 +3,14 @@
 
 import sys
 import numpy
-
+import random
 from termcolor import colored
 
 
 #count array
-numbers = [
-    "1",
-    "2",
-    "3",
-    "4",
-    "5",
-    "6",
-    "7",
-    "8",
-    "9",
-    "10",
-    "11",
-    "12",
-    "13",
-    "14",
-    "15",
-    "16",
-    "17",
-    "18",
-    "19",
-    "20",
-    "21",
-    "22",
-    "23",
-    "24",
-    "25",
-    "26",
-    "27",
-    "28",
-    "29",
-    "30",
-    "31",
-    "32",
-    "33",
-    "34",
-    "35",
-    "36",
-    "37",
-    "38",
-    "39",
-    "40",
-    "41",
-    "42",
-    "43",
-    "44",
-    "45",
-    "46",
-    "47",
-    "48",
-    "49",
-    "50",
-    "51",
-    "52",
-    "53",
-    "54",
-    "55",
-    "56",
-    "57",
-    "58",
-    "59",
-    "60",
-    "61",
-    "62",
-    "63",
-    "64",
-    "65",
-    "66",
-    "67",
-    "68",
-    "69",
-    "70",
-    "71",
-    "72",
-    "73",
-    "74",
-    "75",
-    "76",
-    "77",
-    "78",
-    "79",
-    "80",
-    "81",
-    "82",
-    "83",
-    "84",
-    "85",
-    "86",
-    "87",
-    "88",
-    "89",
-    "90",
-    "91",
-    "92",
-    "93",
-    "94",
-    "95",
-    "96",
-    "97",
-    "98",
-    "99",
-    "100"
-]
-
-
+numbers = [str(x) for x in range(0, 100)]
+colors = ["red", "white", "blue"]
 sortnumber = []
-
 
 def quicksort(arr):
     if len(arr) <= 1:
@@ -126,7 +23,6 @@ def quicksort(arr):
     
     return quicksort(left) + middle + quicksort(right)
 
-
 def print1():
     # print counted numbers in array
     for number in range(len(numbers)):
@@ -136,7 +32,7 @@ def print1():
             output = str(output + str(numbers[number][n]))
     
         # print in terminal
-        print(colored(f"{str(int(output))}", "red"))
+        print(colored(f"{str(int(output))}", colors[random.randint(0,2)]))
 
 
 def print2():
@@ -150,7 +46,7 @@ def print2():
             output = str(output + str(numbers[sortnumber][n]))
         
         # print in terminal
-        print(colored(f"{str(int(output))}", "red"))
+        print(colored(f"{str(int(output))}", colors[random.randint(0,2)]))
 
 
 def main():
@@ -160,5 +56,5 @@ def main():
 
 if __name__ == "__main__":
     main()
-    sys.exit()
+
 
